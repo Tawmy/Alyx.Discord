be480325084865cafc21001759c6a73465d8669d
Tawmy <git@tawmy.dev>
2024-09-21 00:17:24 +0200

    Fix active job level bg after library update

--- Alyx.Discord.Core/StaticValues/CharacterSheetValues.cs
+++ Alyx.Discord.Core/StaticValues/CharacterSheetValues.cs
@@ -5,8 +5,8 @@ namespace Alyx.Discord.Core.StaticValues;
 public static class CharacterSheetValues
 {
     public static Rgba32 ActiveJobLevelBackground => new(0.2f, 0.2f, 0.2f);
-    public static int ActiveJobLevelThickness => 40;
-    public static int ActiveJobLevelRadius => 10;
+    public static int ActiveJobLevelThickness => 58;
+    public static int ActiveJobLevelRadius => 1;
     public static int ActiveJobLevelFontSize => 30;
     public static int FontSizeHomeWorld => 28;
     public static int FontSizeMiMo => 32;
