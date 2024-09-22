856361e7e47a779f0594c816128a579c2ea0e086
Tawmy <git@tawmy.dev>
2024-09-22 14:51:59 +0200

    Reply with character sheet after character selection

--- Alyx.Discord.Bot/ComponentInteractionHandler/SelectCharacterHandler.cs
+++ Alyx.Discord.Bot/ComponentInteractionHandler/SelectCharacterHandler.cs
@@ -1,19 +1,38 @@
 using Alyx.Discord.Bot.Interfaces;
+using Alyx.Discord.Core.Requests.Character.Sheet;
 using DSharpPlus;
 using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
-using NetStone.Api.Client;
+using MediatR;
+using SixLabors.ImageSharp.Formats.Webp;
 
 namespace Alyx.Discord.Bot.ComponentInteractionHandler;
 
-public class SelectCharacterHandler(NetStoneApiClient client) : IComponentInteractionHandler
+public class SelectCharacterHandler(ISender sender) : IComponentInteractionHandler
 {
-    public async Task HandleAsync(DiscordClient sender, ComponentInteractionCreatedEventArgs args)
+    public async Task HandleAsync(DiscordClient discordClient, ComponentInteractionCreatedEventArgs args)
     {
         await args.Interaction.DeferAsync(true);
-        var selected = args.Values.First();
-        var dto = await client.Character.GetAsync(selected);
-        var builder = new DiscordFollowupMessageBuilder().WithContent($"{dto.Name} ({dto.Server})."); // TODO
+        var selectedLodestoneId = args.Values.First();
+
+        var sheet = await sender.Send(new CharacterSheetRequest(selectedLodestoneId));
+
+        await using var stream = new MemoryStream();
+        await sheet.SaveAsync(stream, new WebpEncoder());
+        stream.Seek(0, SeekOrigin.Begin);
+
+        var fileName = $"{DateTime.UtcNow:yyyy-MM-dd HH-mm} {selectedLodestoneId}.webp";
+
+        var button = CreateLodestoneLinkButton(selectedLodestoneId);
+
+        var builder = new DiscordFollowupMessageBuilder().AddFile(fileName, stream, true).AddComponents(button);
+
         await args.Interaction.CreateFollowupMessageAsync(builder);
+    }
+
+    private static DiscordLinkButtonComponent CreateLodestoneLinkButton(string characterId)
+    {
+        var url = $"https://eu.finalfantasyxiv.com/lodestone/character/{characterId}";
+        return new DiscordLinkButtonComponent(url, "Open Lodestone profile");
     }
 }
