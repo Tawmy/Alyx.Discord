50a727d1aa2935ddff73b9cf89b5805a13ca50bf
Tawmy <git@tawmy.dev>
2024-10-01 10:13:17 +0200

    Initialise ExternalResourceService during startup

--- Alyx.Discord.Core/Requests/Ffxiv/Copypasta/FfxivCopypastaRequestHandler.cs
+++ Alyx.Discord.Core/Requests/Ffxiv/Copypasta/FfxivCopypastaRequestHandler.cs
@@ -6,10 +6,10 @@ namespace Alyx.Discord.Core.Requests.Ffxiv.Copypasta;
 internal class FfxivCopypastaRequestHandler(ExternalResourceService externalResourceService)
     : IRequestHandler<FfxivCopypastaRequest, FfxivCopypastaRequestResponse>
 {
-    public async Task<FfxivCopypastaRequestResponse> Handle(FfxivCopypastaRequest request,
+    public Task<FfxivCopypastaRequestResponse> Handle(FfxivCopypastaRequest request,
         CancellationToken cancellationToken)
     {
-        var image = await externalResourceService.GetOtherImageAsync("copypasta");
-        return new FfxivCopypastaRequestResponse(image);
+        var image = externalResourceService.GetOtherImage("copypasta");
+        return Task.FromResult(new FfxivCopypastaRequestResponse(image));
     }
 }
