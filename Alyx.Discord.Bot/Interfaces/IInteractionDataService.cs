557744f9927677d34e0fb56d8761159abef9505f
Tawmy <git@tawmy.dev>
2025-10-28 10:03:02 +0100

    Add cancellation token support to InteractionDataService

--- Alyx.Discord.Bot/Interfaces/IInteractionDataService.cs
+++ Alyx.Discord.Bot/Interfaces/IInteractionDataService.cs
@@ -8,18 +8,19 @@ internal interface IInteractionDataService
     ///     Add data to cache. A new key identifying the object in the database will be returned.
     /// </summary>
     /// <param name="data">Data to cache.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     /// <typeparam name="T">Type of data to cache. Data is serialized and saved to database as json.</typeparam>
     /// <returns>Newly generated key identifying the object in the database.</returns>
     /// <exception cref="ArgumentNullException">Thrown if <see cref="data" /> is null.</exception>
     /// <exception cref="UniqueConstraintException">
     ///     Thrown if data for key already exists. Should never happen as key is automatically generated.
     /// </exception>
-    Task<Guid> AddDataAsync<T>(T data);
+    Task<Guid> AddDataAsync<T>(T data, CancellationToken cancellationToken = default);
 
-    /// <inheritdoc cref="AddDataAsync{T}(T)" />
+    /// <inheritdoc cref="AddDataAsync{T}(T,CancellationToken)" />
     /// <returns>Data Component ID to be used for Discord components. Format: ComponentId/DataKey</returns>
     /// <remarks>There is no overload for submitting data ID. It must always be automatically generated.</remarks>
-    Task<string> AddDataAsync<T>(T data, string componentId);
+    Task<string> AddDataAsync<T>(T data, string componentId, CancellationToken cancellationToken = default);
 
     /// <summary>
     ///     Creates a new component ID by replacing an existing data component ID's component ID with a new one.
@@ -34,19 +35,21 @@ internal interface IInteractionDataService
     ///     Get data for given ID and remove from cache.
     /// </summary>
     /// <param name="key">Key previously returned by <see cref="AddDataAsync{T}(T)" />.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     /// <typeparam name="T">Data type. Data is serialized and saved to database as json.</typeparam>
     /// <returns>Data from database.</returns>
     /// <exception cref="ArgumentException">Thrown if type does not match object.</exception>
     /// <exception cref="InvalidOperationException">Thrown data not found or could not be deserialized.</exception>
-    Task<T> GetDataAsync<T>(string key);
+    Task<T> GetDataAsync<T>(string key, CancellationToken cancellationToken = default);
 
     /// <summary>
     ///     Parse component string and return data.
     /// </summary>
     /// <param name="componentString">Component string with Component ID and Data key. Format: ComponentId/DataKey</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
     /// <typeparam name="T">Data type. Data is serialized and saved to database as json.</typeparam>
     /// <returns>Data from database.</returns>
     /// <exception cref="ArgumentException">Thrown if type does not match object.</exception>
     /// <exception cref="InvalidOperationException">Thrown data not found or could not be deserialized.</exception>
-    Task<T> ParseStringAndGetData<T>(string componentString);
+    Task<T> ParseStringAndGetData<T>(string componentString, CancellationToken cancellationToken = default);
 }
