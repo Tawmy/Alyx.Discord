4cf4b170d50efc63bdc004b6538445e9f2bf1d3e
Tawmy <git@tawmy.dev>
2025-10-28 22:27:54 +0100

    Replace migration with v1.9.0 migration

--- Alyx.Discord.Db/Migrations/20251028212644_v1.9.0.cs
+++ Alyx.Discord.Db/Migrations/20251028212644_v1.9.0.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Alyx.Discord.Db.Migrations
 {
     /// <inheritdoc />
-    public partial class LastForceRefreshFreeCompany : Migration
+    public partial class v190 : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
