32e12339991a71fc6b0d339f23fe64ac9b5f2694
Tawmy <git@tawmy.dev>
2025-12-28 10:21:03 +0100

    Add support for custom Discord health check timeout

--- Alyx.Discord.Bot/EnvironmentVariables.cs
+++ Alyx.Discord.Bot/EnvironmentVariables.cs
@@ -4,4 +4,5 @@ public static class EnvironmentVariables
 {
     public const string BotToken = "BOT_TOKEN";
     public const string DebugGuildId = "DEBUG_GUILD_ID";
+    public const string DiscordHealthCheckTimeout = "DISCORD_HEALTH_CHECK_TIMEOUT";
 }
--- Alyx.Discord.Bot/HealthChecks/DiscordHealthCheck.cs
+++ Alyx.Discord.Bot/HealthChecks/DiscordHealthCheck.cs
@@ -1,21 +1,24 @@
 using System.Diagnostics;
 using DSharpPlus;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace Alyx.Discord.Bot.HealthChecks;
 
-public class DiscordHealthCheck(DiscordClient client) : IHealthCheck
+public class DiscordHealthCheck(DiscordClient client, IConfiguration configuration) : IHealthCheck
 {
     public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
         CancellationToken cancellationToken = default)
     {
+        var timeoutStr = configuration[EnvironmentVariables.DiscordHealthCheckTimeout];
+        var timeout = timeoutStr is not null ? int.Parse(timeoutStr) : 1000;
         try
         {
             var stopwatch = Stopwatch.StartNew();
             await client.GetUserAsync(111462437164724224, true);
             stopwatch.Stop();
 
-            return stopwatch.ElapsedMilliseconds > 1000 ? HealthCheckResult.Degraded() : HealthCheckResult.Healthy();
+            return stopwatch.ElapsedMilliseconds > timeout ? HealthCheckResult.Degraded() : HealthCheckResult.Healthy();
         }
         catch
         {
